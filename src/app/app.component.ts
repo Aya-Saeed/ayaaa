89ec6cd0a73140aaf6b2c8679c61bee71df8a80e
Aya-Saeed <ayariad12@gmail.com>
2022-02-08 16:51:38 +0200

    aya

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Product } from './_models/product.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'agular-project';
+
+
+  addedProduct : Product[] =[];
+  
+  onItemAdd(product : Product){
+    const index = this.addedProduct.findIndex(p => p.data[0].name == product.data[0].name)
+    console.log(index);
+    
+    // if(index >= 0){
+    //   this.addedProduct[index].count++
+    // }else{
+    // this.addedProduct.push(product);}
+// this.addedProduct=product
+  }
+  
+}
